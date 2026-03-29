e2f61e1dcd5edb8a23aa2f0b569e52bc1467fe41
Mtellez93 <ing.miguelangeltellez@gmail.com>
2026-03-29 09:28:29 -0700

    Create templates.js

---
+++ public/templates.js
@@ -0,0 +1,14 @@
+const templates = {
+  birthday: {
+    background: "#fde68a",
+    objects: [
+      {
+        type: "text",
+        text: "🎂 Feliz Cumpleaños",
+        left: 100,
+        top: 40,
+        fontSize: 30
+      }
+    ]
+  }
+};
